929acf0d0f35c69e063a474eca7881583fc2c8e1
gregmac <github@list.mtechsolutions.ca>
2012-02-09 02:16:44 -0500

    Add array samples

---
+++ src/NServiceMVC.Examples.HelloWorld/Controllers/ArraySampleController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AttributeRouting;
+
+namespace NServiceMVC.Examples.HelloWorld.Controllers
+{
+    public class ArraySampleController : ServiceController
+    {
+        //
+        // GET: /ArraySample/
+
+        [GET("arraysample/string/list")]
+        public List<string> StringList()
+        {
+            return new List<string>() { "one", "two", "three" };
+        }
+
+        [GET("arraysample/string/enumerable")]
+        public IEnumerable<string> StringEnumerable()
+        {
+            return new List<string>() { "one", "two", "three" };
+        }
+
+        [GET("arraysample/string/array")]
+        public string[] StringArray()
+        {
+            return new string[] { "one", "two", "three" };
+        }
+
+    }
+}
